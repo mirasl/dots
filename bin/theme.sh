bb8bdf31f025534ee89f24b70b0d9d7feaa14279
mirasl <santaluciamira+chia@gmail.com>
2022-07-21 14:03:14 -0700

    Added automated alacritty theme-switching to theme.sh

--- bin/theme.sh
+++ bin/theme.sh
@@ -4,6 +4,7 @@ then
   if [ $1 -lt 7 ]
   then
     printf "local current_theme = $1\nreturn current_theme" > ~/.config/awesome/current_theme.lua;
+    sed -i "/colors: */c\colors: *$1" ~/.config/alacritty/alacritty.yml
     echo "awesome.restart()" | awesome-client;
   else
     echo "Error - number out of range"
